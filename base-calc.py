78802992d78af50faa92e0b77159bb2b7e53d8f2
TotalmenteUnRomanxd <romantragacanelones143@gmail.com>
2026-05-14 16:57:49 -0300

    Add menu for operation selection in base-calc.py

--- base-calc.py
+++ base-calc.py
@@ -1,3 +1,21 @@
+menu():
+ print("Bienvenido. Aqui tienes las opciones:")
+ print("1. Suma                      2. Resta")
+ print("3. Multiplicacion            4. Division")
+ mango=input(int())
+if mango=1:
+ print("Utilize suma(*primer numero*, *segundo numero*) para la operacion")
+else:
+    if mango=2:
+     print("Utilize resta(*primer numero*, *segundo numero*) para la operacion")
+    else:
+        if mango=3:
+            print("Utilize multiplicacion(*primer numero*, *segundo numero*) para la operacion")
+        else:
+            if mango=4:
+                print("Utilize division(*primer numero*, *segundo numero*) para la operacion")
+            else:
+                print("No es una opcion")       
 def suma(a, b):
     return a+b
 def resta(a, b):
